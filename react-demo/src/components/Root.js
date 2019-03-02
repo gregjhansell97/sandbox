c5a883862c5383ee31f9bb7431c301ab7f154c11
randypowell10 <randypowell10@yahoo.com>
2019-03-02 16:59:39 -0500

    Restaurant Dictionary List and Stuff

--- react-demo/src/components/Root.js
+++ react-demo/src/components/Root.js
@@ -3,13 +3,34 @@ import PropTypes from "prop-types";
 
 //inhouse
 import RestaurantCard from "./RestaurantCard"
+import AppBar from './AppBar';
 
 class Root extends React.Component {
-  render() {
-    return (
-      <RestaurantCard name="Moes" description="burrito place"/>
-    );
-  }
+
+    render() {
+        
+        let restaurants = [
+            {name:'kekes', description:'tacos'}, 
+            {name:'chipotle', description: 'burritos'}, 
+            {name:'Qdoba', description: 'ripoff chipotle'}, 
+            {name:'Moes', description: 'more burritos and shit'}, 
+            {name:'Taco Bell', description: 'shit'}
+        ];
+
+        return (
+            <div>
+                <div>
+                    <AppBar name="Restaurants"></AppBar>
+                </div>
+                <div>
+                    {restaurants.map((r) =>
+                        <RestaurantCard name={r.name} description={r.description}></RestaurantCard>
+                    )
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Root;
