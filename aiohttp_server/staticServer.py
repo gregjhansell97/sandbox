589d16f7ed567ca6838726e9980baa3ace20c708
Alex Elias <aelias36@yahoo.com>
2019-03-02 22:30:21 -0500

    Added static server and redirect page

---
+++ aiohttp_server/staticServer.py
@@ -0,0 +1,33 @@
+'''
+https://docs.aiohttp.org/en/stable/web_quickstart.html
+
+'''
+
+from aiohttp import web
+import aiohttp_cors
+
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+
+class SimpleServer:
+    def run(self):
+        app = web.Application()
+        cors = aiohttp_cors.setup(app, defaults={
+            "*": aiohttp_cors.ResourceOptions(
+                    allow_credentials=True,
+                    expose_headers="*",
+                    allow_headers="*",
+                )
+        })
+        route = app.router.add_static('/', 'public/')
+        #cors.add(route)
+
+        # https://github.com/aio-libs/aiohttp-cors/issues/151
+        for resource in list(app.router.resources()):
+            cors.add(resource)
+        web.run_app(app)
+
+if __name__ == "__main__":
+    server = SimpleServer()
+    server.run()
